7f5500032dda02a971337c3257b8dede93f5e8d2
rana waleed <awaiseboy@gmail.com>
2025-08-14 01:50:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const FIVERR_URL = "https://www.fiverr.com/"; // Replace with your Fiverr gig link
+const YOUR_EMAIL = "example@email.com"; // Replace with your email
+
+document.getElementById("emailLink").href = `mailto:${YOUR_EMAIL}`;
